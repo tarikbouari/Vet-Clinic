379cc6cb4f2b37a0157d80d04f5dc68e1897d506
Rikstone <tarikbouari@gmail.com>
2022-10-27 13:08:20 +0000

    make queries

--- queries.sql
+++ queries.sql
@@ -12,4 +12,26 @@ UPDATE animals SET species = 'unspecified';
 UPDATE animals SET species ='';
 
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-UPDATE animals SET species = 'pokemon' WHERE species ='';
+UPDATE animals SET species = 'pokemon' WHERE species ='';UPDATE animals SET species = 'pokemon' WHERE species ='';
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT my_save_point1;
+UPDATE animals SET weight_kg = weight_kg* -1;
+
+ROLLBACK TO my_save_point1;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0; 
+
+SELECT * FROM animals;
+
+COMMIT; 
+
+SELECT COUNT(*) AS All_animal FROM  animals;
+
+SELECT AVG(weight_kg) FROM animals;
+SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
+SELECT MIN(weight_kg), MAX(weight_kg), species FROM animals GROUP BY species;
+SELECT SUM(escape_attempts), neutered FROM animals GROUP BY neutered;
+SELECT AVG(escape_attempts), species, date_of_birth FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species, date_of_birth;
